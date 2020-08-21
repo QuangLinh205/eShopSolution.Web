c72ec5325b6870aa0ee8c70d8e9ce54e8429b311
Nguyen Quang Linh <quanglinh20598@gmail.com>
2020-08-21 10:12:08 +0700

    Identity

---
+++ eShopSolution.Data/Migrations/20200821025426_AspNetCoreIdentityDatabase.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class AspNetCoreIdentityDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppUserRoles",
+                table: "AppUserRoles");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppUserRoles",
+                table: "AppUserRoles",
+                columns: new[] { "UserId", "RoleId" });
+
+            migrationBuilder.UpdateData(
+                table: "AppRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"),
+                column: "ConcurrencyStamp",
+                value: "ff7b336d-0a5a-4706-8f79-150cd5f0a0dc");
+
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "130e3882-75f0-4094-884b-ca1ff623748e", "AQAAAAEAACcQAAAAEK+egxlx+MxsZbbchDTd9GjqW09PAkgRsj+75VHPhmTdUgx5uXcTT5YlTdIFubtbEQ==" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2020, 8, 21, 9, 54, 25, 436, DateTimeKind.Local).AddTicks(4089));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppUserRoles",
+                table: "AppUserRoles");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppUserRoles",
+                table: "AppUserRoles",
+                column: "UserId");
+
+            migrationBuilder.UpdateData(
+                table: "AppRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"),
+                column: "ConcurrencyStamp",
+                value: "46f4594b-f8d1-46d1-a8c7-bf469afd85eb");
+
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "45fe5c48-4ddc-4b8d-bd35-ac56c4cd1ca1", "AQAAAAEAACcQAAAAEBPGFHy29TRtRkCYwMgNEFLw+CiEw7kxusnyvVMKehZmllqKJ/E/YbdkqXV14mw1WQ==" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2020, 8, 21, 9, 50, 8, 484, DateTimeKind.Local).AddTicks(6332));
+        }
+    }
+}
